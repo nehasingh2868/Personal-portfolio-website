0d15bd8a0038504a882cf2ffb0edb5afbc7834db
Neha Singh <146656729+nehasingh2868@users.noreply.github.com>
2023-10-18 13:06:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// let nameElement = document.querySelector('.js-name')
+// let name2 = nameElement.value ;
+
+// let emailElement = document.querySelector('.js-email')
+// let email = emailElement.value;
+
+// let contactElement = document.querySelector('.js-contact')
+// let contact = contactElement.value;
+
+// let messageElement = document.querySelector('.js-message')
+// let message = messageElement.value;
+
+// let data = [{
+//   name: `${name2}`,
+//   email: `${email}`,
+//   contact: `${contact}`,
+//   message: `${message}`
+// }];
+
+// function submitData(){
+//   data.name=nameElement.value;
+//   data.email = emailElement.value;
+//   data.contact = Number(contactElement.value)
+//   data.message = messageElement.value
+// }
+
+
+
+
+// document.querySelector('.js-submit-btn').addEventListener('click', ()=>{
+//   submitData();
+// })
+
+
+document.querySelector(".js-project-btn").addEventListener('click', ()=>{
+  documen
+  
+}
+)
+  
+
+
+
+
+
+
+
+
