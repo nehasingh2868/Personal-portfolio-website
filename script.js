e8b8cd121e66efe175cfbe81d336bb2da567656e
Neha Singh <146656729+nehasingh2868@users.noreply.github.com>
2023-10-23 15:56:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,12 @@
-// let nameElement = document.querySelector('.js-name')
-// let name2 = nameElement.value ;
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+logo = document.querySelector('.logo')
+navlist = document.querySelector('.navitems')
 
-// let emailElement = document.querySelector('.js-email')
-// let email = emailElement.value;
-
-// let contactElement = document.querySelector('.js-contact')
-// let contact = contactElement.value;
-
-// let messageElement = document.querySelector('.js-message')
-// let message = messageElement.value;
-
-// let data = [{
-//   name: `${name2}`,
-//   email: `${email}`,
-//   contact: `${contact}`,
-//   message: `${message}`
-// }];
-
-// function submitData(){
-//   data.name=nameElement.value;
-//   data.email = emailElement.value;
-//   data.contact = Number(contactElement.value)
-//   data.message = messageElement.value
-// }
-
-
-
-
-// document.querySelector('.js-submit-btn').addEventListener('click', ()=>{
-//   submitData();
-// })
-
-
-document.querySelector(".js-project-btn").addEventListener('click', ()=>{
-  documen
-  
-}
-)
-  
-
-
-
-
-
-
+burger.addEventListener('click', ()=>{
+  navlist.classList.toggle('v-class-resp');
+  logo.classList.toggle('v-class-resp');
+  navbar.classList.toggle('h-nav-resp');
+})
 
 
